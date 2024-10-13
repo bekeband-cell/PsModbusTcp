7eebad92c865ec1d05ec5b9d653116864563fa9b
Beke András <beke.andras.mkolc@gmail.com>
2024-10-13 22:23:25 +0200

    commit

---
+++ MODBUS-TCP-LOG.ps1
@@ -0,0 +1,144 @@
+# "Functions loading."
+. .\PsModbusTcp.ps1
+
+#Arguments from command line. These are default value as well.
+$computername = "localhost"
+$portnumber = "502"
+$MODBUS_address = '0'
+$output_directory = ".\DATAS\"
+$dirname = "yyyyMM"
+$filename = $dirname + "dd"
+$samplesec = "1"    # sample times (1..30 x10 sec)
+$samplecount = "30" # data save in sample times(6..24 x sample time)
+
+$datacounter = 0
+
+# We are assume that 8 channels datas from MODBUS.
+# $fileheader = "Date`tChan1`tChan2`tChan3`tChan4`tChan5`tChan6`tChan7`tChan8"
+$fileheader = "Date`tChan1`tChan2`tChan3"
+
+$stopwatch = [system.diagnostics.stopwatch]::StartNew()
+#$stopwatch.Elapsed
+
+function MinMaxSetting($min, $max, $value) {
+    if (($value -gt $max)) {
+        return $max
+    }
+    else {
+        if ($value -lt $min) {
+            return $min;
+        }
+        else {
+            return $value
+        }
+    }
+ 
+}
+
+Function Quit($Text) {
+    Write-Host "Quiting because: " $Text
+    Break Script
+} 
+
+for ( $i = 0; $i -lt $args.count; $i++ ) {
+    if ($args[ $i ] -eq "-c") { $computername = $args[ $i + 1 ] }
+    if ($args[ $i ] -eq "-p") { $portnumber = $args[ $i + 1 ] }
+    if ($args[ $i ] -eq "-d") { $dirname = $args[ $i + 1 ] }
+    if ($args[ $i ] -eq "-f") { $filename = $args[ $i + 1 ] }
+    if ($args[ $i ] -eq "-g") { 
+        $samplesec = int(MinMaxSetting(1, 30, $args[ $i + 1 ]))
+    }
+    if ($args[ $i ] -eq "-j") { 
+        $samplecount = int(MinMaxSetting(6, 24, $args[ $i + 1 ])) 
+    }
+    if ($args[ $i ] -eq "-k") { $fileheader = $args[ $i + 1 ] }
+    if ($args[ $i ] -eq "-h") { 
+        Quit("never mind.")
+    }
+}
+
+"MODBUS READING PROGRAM START:" 
+"Date and time is: $((Get-Date).ToString())"
+
+# .\Path
+function MakePathIfNoExist {
+    param (
+        [Parameter(Mandatory = $true)][string]$PathString
+    )
+    if (Test-Path -Path "$PathString") {
+        "$PathString exists!"
+    }
+    else {
+        "$YMdir doesn't exist Create:"
+        New-Item -Path "$PathString" -ItemType Directory
+
+    }
+}
+
+do {
+    #    Test the computer ping.
+    #    Write-Host "MODBUS cliens pinging " $computername
+    #    $test_ping = Test-Connection -Count 1 -Delay 1 -Quiet -ComputerName $computername
+    #    $test_ping = Test-NetConnection -Port $portnumber -ComputerName $computername
+    #    Write-Host "Ping succeed." -ForegroundColor Green 
+    #    $test_ping = Test-Connection -Count 1 -Delay 1 -Quiet -ComputerName $computername
+    Start-Sleep -Milliseconds 1000
+    Write-Host "$computername pinging."
+    $test_ping = Test-Connection -Count 1 -Delay 1 -Quiet -ComputerName $computername
+
+    if ($test_ping) {
+        Write-Host "$computername ping." -NoNewline
+        Write-Host " OK." -ForegroundColor Green
+        $read_return = Read-HoldingRegisters -Address $computername -Port $portnumber -Reference $MODBUS_address -Num 8
+            
+        if ($null -ne $read_return) {
+            Write-Host "Succesfully read from MODBUS client." -ForegroundColor Green
+
+            if ($datacounter -lt $samplecount) {
+                $datacounter++
+            }
+            else {
+
+            }
+
+            Write-Host "Computer: " $computername " Date: " $((Get-Date).ToString())
+            Write-Host "Readed data: " $read_return
+            # here to write to file
+            # filename making
+                 
+            $YMdir = $output_directory + (Get-Date).tostring($dirname)
+            $YMDdir = $YMdir + '\' + (Get-Date).tostring($filename)
+                
+            $logoname = $YMDdir + '.csv'
+
+            MakePathIfNoExist -PathString "$YMdir"
+
+            if (Test-Path -Path "$logoname") {
+                "$logoname exists!"
+            }
+            else {
+                New-Item -Path . -Name "$logoname" -ItemType "file" -Value "$fileheader"
+                #                MakePathIfNoExist -PathString "$logoname"
+            }
+
+        }
+        else {
+            Write-Host "No read from MODBUS client." -ForegroundColor Red
+            $cont_dataread = 0
+        }
+
+    }
+    else {
+        Write-Host "$computername ping" -NoNewline
+        Write-Host "Eerror!" -ForegroundColor Red
+    }
+    #Write-Host "MODBUS cliensek pingelése " + $servers[$i]
+    #    Test-Connection -ComputerName $servers[$i] -Count 2
+
+    
+
+}while (1)
+
+
+
+#Read-HoldingRegisters -Address 127.0.0.1 -Port 502 -Reference 0 -Num 8
--- README.md
+++ README.md
@@ -3,3 +3,14 @@ A set of simple Powershell methods to work with MODBUS protocol over TCP transpo
 
 ## Status
 Only reading/writing registers implemented at this point. This is used mainly fro research/dev work. Code may be updated if a need arises. Pull requests are welcome.
+
+
+Parancssoros opciók:
+
+-c computername 'A MODBUS eszköz neve, vagy IP címe. Alapérték: "localhost"'
+-p portnumber A MODBUS/TCP port száma: Alapérték: 502
+-d dirname A kimeneti könyvtár nevének mintája. 
+-f filename 
+-g samplesec Az adatok lekérdezédének gyakorisága 10 másodpercekben. Határok: 1...30, azaz a gyakoriságot 10 és 300 másodperc között lehet állítani.
+-j samplecount Az adatok gyűjtésének és mentésének gyakorisága a lekérdezések függvényéban. Határok: 6..24. A trend mentés és adatgyűjtés kiszámítása: adatmentés ideje samplecount * samplesec * 10. Pl.: samplesec = 2 samplecount = 10 Így az adatgyűjtés ideje 2 * 10 * 10 = 200 sec = 3 perc 20 mp. 
+-k 
