9dcb2892beda1165020b99bcb08196ceedb7d00b
Beke András <beke.andras.mkolc@gmail.com>
2024-10-21 22:45:46 +0200

    reeew

--- testdatas.ps1
+++ testdatas.ps1
@@ -5,19 +5,23 @@ $ma20_values = 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000
 $dim0mAvalues = 0, 0, 0, 0, 0, 0, 0, 0
 $dim20mAvalues = 0.4, 0.3, 10, 1.3, 0.3, 0.3, 0.4, 0.5
 $samplechannels = 8
+$samplecount = 3
 
-$channel_datas[, ] = 1500, 2230, 3350, 4000
-
-
+$channel_datas = New-Object 'object[,]' $samplechannels, $samplecount
+$channel_datas[0, 0] = 7.7865
 
 do {
 
-    for ($i = 1; $i -le $samplechannels; $i++) {
-        #        $channel_datas[$i][0] 
+    for ($i = 0; $i -lt $samplechannels; $i++) {
+
+        for ($j = 0; $j -lt $samplecount; $j++) {
+            Write-Host "$i , $j : $channel_datas "
+           
+        }
+
     }
     #    $channel_datas[1][1] = 10
     #    Start-Sleep -Milliseconds (100)
 
-    Write-Host "Channel Datas: $channel_datas[0][0]"
 
-}while (0)
+} while (0)
