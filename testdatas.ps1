36b97cefbaf25a17910835ee7028dd83157ef076
Beke András <beke.andras.mkolc@gmail.com>
2024-10-21 16:17:20 +0200

    rdaea

---
+++ testdatas.ps1
@@ -0,0 +1,23 @@
+
+
+$ma4_values = 0, 0, 0, 0, 0, 0, 0
+$ma20_values = 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000
+$dim0mAvalues = 0, 0, 0, 0, 0, 0, 0, 0
+$dim20mAvalues = 0.4, 0.3, 10, 1.3, 0.3, 0.3, 0.4, 0.5
+$samplechannels = 8
+
+$channel_datas[, ] = 1500, 2230, 3350, 4000
+
+
+
+do {
+
+    for ($i = 1; $i -le $samplechannels; $i++) {
+        #        $channel_datas[$i][0] 
+    }
+    #    $channel_datas[1][1] = 10
+    #    Start-Sleep -Milliseconds (100)
+
+    Write-Host "Channel Datas: $channel_datas[0][0]"
+
+}while (0)
